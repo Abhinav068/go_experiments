980a5daacccc44fa94f27f02f22c3c9d4fd98193
Abhinav068 <abhinav28897@gmail.com>
2025-07-27 21:53:24 +0530

    any type manipulations

---
+++ dockfile/main.go
@@ -0,0 +1,17 @@
+// main.go
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello from Docker!")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	fmt.Println("Server running on port 8080")
+	http.ListenAndServe(":8080", nil)
+}
--- main.go
+++ main.go
@@ -1,5 +1,40 @@
 package main
 
+import "fmt"
+
 func main() {
-	
+	integer := 5
+	slice := []int{5}
+	str := "abhinav"
+
+	change_integer(&integer)
+	change_slice(slice)
+	change_string(&str)
+	fmt.Println("integer after: ", integer)
+	fmt.Println("slice after: ", slice)
+	fmt.Println("str after: ", str)
+}
+
+func change_integer(inp any) {
+	fmt.Println("change_integer started: ", inp)
+	// *inp.(int) = 6 // this is not valid syntax, because inp.(int) is a copy, not a variable, so you can't dereference it.
+
+	// in order to change a concrete type value you need to pass its reference, so that you could do this:
+	*inp.(*int) = 7
+}
+
+func change_slice(inp any) {
+	fmt.Println("change_slice started: ", inp)
+	inp.([]int)[0] = 4 // no need to put * in type conversion ie. inp.(*[]int). Slice is already a pointer type.
+	val, ok := inp.([]int)
+	if !ok {
+		fmt.Println("something's wrong")
+	}
+	fmt.Println(val)
+
+}
+
+func change_string(inp any) {
+	fmt.Println("change_string started: ", inp)
+	*inp.(*string) = "aman"
 }
