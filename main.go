861955cebb5a167a016c489867d60d60707ac5ad
Abhinav068 <abhinav28897@gmail.com>
2025-08-04 18:29:06 +0530

    basic server in go

--- main.go
+++ main.go
@@ -4,53 +4,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"time"
 )
 
-type Cache struct {
-	Data   map[string]string
-	Expiry map[int64][]string
-}
-
-func (ch *Cache) Set(inp, val string, exp int64) {
-	if inp != "" && val != "" {
-		ch.Data[inp] = val
-	}
-	if exp > 0 {
-		ch.Expiry[exp] = append(ch.Expiry[exp], inp)
-	}
-}
-
-func (ch *Cache) Get(inp string) string {
-	return ch.Data[inp]
-}
-
-var cache Cache
-var CurrTime int64
-
 func main() {
-	cache = Cache{
-		Data:   map[string]string{},
-		Expiry: map[int64][]string{},
-	}
-	CurrTime = time.Now().Unix()
-	go func() {
-		for {
-			<-time.Tick(time.Second)
-			CurrTime += int64(time.Second)
-			cacheDataKeys := cache.Expiry[CurrTime]
-			fmt.Println("CurrTime", CurrTime)
-			if len(cacheDataKeys) > 0 {
-				fmt.Println(cacheDataKeys)
-				for i := range cacheDataKeys {
-					delete(cache.Data, cacheDataKeys[i])
-				}
-				delete(cache.Expiry, CurrTime)
-			}
-		}
-	}()
 	http.HandleFunc("/set", cacheFn)
-	http.HandleFunc("/get", getFn)
+	// http.HandleFunc("/get", getFn)
 	http.ListenAndServe(":8888", nil)
 }
 func cacheFn(res http.ResponseWriter, req *http.Request) {
@@ -64,22 +22,8 @@ func cacheFn(res http.ResponseWriter, req *http.Request) {
 		http.Error(res, err.Error(), http.StatusBadRequest)
 		return
 	}
-	var expTime int64
-	if kv.Exp > 0 {
-		duration := kv.Exp * int64(time.Second)
-		expTime = CurrTime + int64(time.Duration(duration))
-		cache.Set(kv.Key, kv.Val, expTime)
-	}
 
-	cache.Set(kv.Key, kv.Val, 0)
-	// fmt.Println(cache.Data)
-	// fmt.Println(cache.Expiry)
 	fmt.Println(kv)
-}
-
-func getFn(res http.ResponseWriter, req *http.Request) {
-	val := req.URL.Query()["key"][0]
-	// fmt.Println(val)
-	// fmt.Println(cache.Get(val))
-	res.Write([]byte(cache.Get(val)))
+	val, _ := json.Marshal(kv)
+	res.Write(val)
 }
